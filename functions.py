864c754c228d6122b143afb24975e0bd5e002734
Mbiki-KK <wawerumbiki@gmail.com>
2023-10-14 19:40:11 +0300

    feat: tried function for odd numbers

--- functions.py
+++ functions.py
@@ -2,3 +2,18 @@ def hello_world():
     print("Hello World!")
 
 hello_world()
+
+def add(x,y):
+    z = x+y
+    return z
+
+print(add(1,2))
+
+def odd_numbers():
+    for i in range(1,101,2):
+        print(i)
+        
+
+print(odd_numbers())
+        
+
