53f67327aeab38c8d79ccbdc06ef3f860ab0fe69
Mbiki-KK <wawerumbiki@gmail.com>
2023-10-14 19:02:53 +0300

    feat: Used break

--- string_iteration.py
+++ string_iteration.py
@@ -1,7 +1,13 @@
-name = "Mbiki Waweru"
+name = "Mbiki Waweru "
 
 for i in name:
     if i == " ":
         continue
+    else:
+        print(i)
+
+for i in name:
+    if i == " ":
+        break
     else:
         print(i)
